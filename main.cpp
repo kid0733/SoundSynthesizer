d27d593a01ee80395dd2034a1043cd0f10016a62
Siddhant Karki <siddhantskarki@gmail.com>
2024-04-10 08:11:07 +1000

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// SoundSynthesizer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    return 0;
+
+}
